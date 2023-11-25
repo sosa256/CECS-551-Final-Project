d7f84093c48b308c034e3c1b3f38df906a59034b
sosa256 <esosa256@protonmail.com>
2023-11-24 23:43:13 -0800

    Removing CategoryLvl1Desc from data analysis

--- visualise_data.py
+++ visualise_data.py
@@ -8,7 +8,10 @@ import matplotlib.pyplot as plt
 data_frame = pd.read_csv('./Coffee_Stores_Data.csv')
 
 # Removes last two columns since they are not needed.
-data_frame = data_frame.drop(['GroupID', 'MissedSales'], axis=1)
+# CategoryLvl1Desc - there is only type of label in this column ('Food')
+# GroupID - Instructions say it's not relevent
+# MissedSales - same as above
+data_frame = data_frame.drop(['CategoryLvl1Desc', 'GroupID', 'MissedSales'], axis=1)
 
 # Associate the ItemType lables with a number.
 # 'Unassigned' = 0
@@ -45,6 +48,5 @@ print('amount of unique Dates: %d' % len(data_frame['BusinessDate'].unique()))  
 print('amount of unique Items: %d' % len(data_frame['PLU'].unique()))                       # 31
 print('amount of unique Descriptions: %d' % len(data_frame['Description'].unique()))        # 31
 print('amount of unique Item Typess: %d' % len(data_frame['ItemType'].unique()))            # 3
-print('amount of unique Category desc1: %d' % len(data_frame['CategoryLvl1Desc'].unique())) # 1
 print('amount of unique Category desc1: %d' % len(data_frame['CategoryLvl2Desc'].unique())) # 2
 print('amount of unique Category desc1: %d' % len(data_frame['CategoryLvl3Desc'].unique())) # 7
